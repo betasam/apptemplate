8690e78634a8f1982020d9fc2bfdac2ceab208f1
Sunil Beta <betasam@gmail.com>
2013-08-12 09:02:42 +0530

    formatted:	source according to coding guidelines

--- src/lcache.c
+++ src/lcache.c
@@ -1,6 +1,6 @@
 /**
- * @file	lcache.c
- * @brief	object cache with binary search
+ * @file        lcache.c
+ * @brief       object cache with linear search
  */
 
 #include <stdlib.h>
@@ -9,8 +9,18 @@
 #include <lconst.h>
 #include <lcache.h>
 
-s32_t s32f_lcache_init(  s32_t     s32v_lcache_cnt, u32_t u32v_lcache_flags, 
-			 lcache_t* lcache_ptr )
+/**
+ * @function    s32f_lcache_init
+ * @brief       initialize cache
+ * @param       s32v_lcache_cnt         cache size
+ * @param       s32v_lcache_flags       flags, see: lcache.h
+ * @param       lcache_ptr              cache to initialize
+ *
+ * @return      0 on success, -1 on failure
+ */
+s32_t s32f_lcache_init(  s32_t     s32v_lcache_cnt, 
+                         u32_t     u32v_lcache_flags,
+                         lcache_t* lcache_ptr )
 {
   s32_t s32v_ret = 0;
   u32_t u32v_sz  = 0;
@@ -19,8 +29,8 @@ s32_t s32f_lcache_init(  s32_t     s32v_lcache_cnt, u32_t u32v_lcache_flags,
   do {
 
     if( (s32v_lcache_cnt     <= 0) || 
-	(u32v_lcache_flags   == LCACHE_FLAG_INVALID) || 
-	(lcache_ptr          == 0)) {
+        (u32v_lcache_flags   == LCACHE_FLAG_INVALID) || 
+        (lcache_ptr          == 0)) {
       s32v_ret = -1;
       break;
     }
@@ -56,10 +66,16 @@ s32_t s32f_lcache_init(  s32_t     s32v_lcache_cnt, u32_t u32v_lcache_flags,
 
   } while(0);
   
-
   return s32v_ret;
 }
 
+/**
+ * @function    s32f_lcache_free
+ * @brief       frees memory inside the cache
+ *              does not free the cache, as it is outside scope
+ * @param       lcache_ptr      cache to free
+ * @return      0 on success, -1 on failure
+ */
 s32_t s32f_lcache_free( lcache_t* lcache_ptr )
 {
   s32_t s32v_ret = 0;
@@ -71,7 +87,7 @@ s32_t s32f_lcache_free( lcache_t* lcache_ptr )
     }
 
     if( (lcache_ptr->u32p_hash    == 0) ||
-	(lcache_ptr->u64p_element == 0) ) {
+        (lcache_ptr->u64p_element == 0) ) {
       s32v_ret = -1;
       break;
     }
@@ -88,8 +104,17 @@ s32_t s32f_lcache_free( lcache_t* lcache_ptr )
   return s32v_ret;
 }
 
-s32_t s32f_lcache_add( lcache_t* lcache_ptr, u64_t u64v_value, 
-		       u32_t u32v_hash )
+/**
+ * @function    s32f_lcache_add
+ * @brief       add an item to the cache
+ *              if it exceeds size, wrap around
+ * @param       u64v_value      value to be added
+ * @param       u64v_hash       hash to access value
+ * @return      0 on success, -1 on failure
+ */
+s32_t s32f_lcache_add( lcache_t* lcache_ptr, 
+                       u64_t u64v_value, 
+                       u32_t u32v_hash )
 {
   s32_t s32v_ret = 0;
 
@@ -101,7 +126,7 @@ s32_t s32f_lcache_add( lcache_t* lcache_ptr, u64_t u64v_value,
       break;
     }
 
-    /* check if extents are exceeded */
+    /* check if extents are exceeded inside cache */
     if( lcache_ptr->s32v_last > lcache_ptr->s32v_cnt ) {
       s32v_ret = -1;
       break;
@@ -109,16 +134,16 @@ s32_t s32f_lcache_add( lcache_t* lcache_ptr, u64_t u64v_value,
 
     /* wrap around ring buffer if it has been filled already */
     if( (lcache_ptr->s32v_last  == lcache_ptr->s32v_cnt) &&
-	(lcache_ptr->u8v_filled == 1) ) {
+        (lcache_ptr->u8v_filled == 1) ) {
       lcache_ptr->s32v_last = 0;
     }
 
     /* WARNING! the following needs to be atomic to be threadsafe */
-    lcache_ptr->u64p_element[lcache_ptr->s32v_last] = u64v_value;
-    lcache_ptr->u32p_hash[ lcache_ptr->s32v_last  ] = u32v_hash;
+    lcache_ptr->u64p_element[ lcache_ptr->s32v_last ] = u64v_value;
+    lcache_ptr->u32p_hash[    lcache_ptr->s32v_last ] = u32v_hash;
     lcache_ptr->s32v_last++;
 
-    /* indicate wrap around if that has happened */
+    /* indicate if cache is filled to support wrap-around */
     if( (lcache_ptr->u8v_filled == 0) && 
         (lcache_ptr->s32v_last  == lcache_ptr->s32v_cnt) ) {
       lcache_ptr->u8v_filled = 1;
@@ -129,6 +154,13 @@ s32_t s32f_lcache_add( lcache_t* lcache_ptr, u64_t u64v_value,
   return s32v_ret;
 }
 
+/**
+ * @function    u64f_lcache_find
+ * @brief       searches for an element in the cache
+ * @param       lcache_ptr      cache to be searched
+ * @param       u32v_hash       hash value used for search
+ * @return      u64_t 64-bit value corresponding to element
+ */
 u64_t u64f_lcache_find( lcache_t* lcache_ptr, u32_t u32v_hash )
 {
   u64_t u64v_ret = U64C_ZERO;
@@ -139,12 +171,14 @@ u64_t u64f_lcache_find( lcache_t* lcache_ptr, u32_t u32v_hash )
       break;
     }
 
-    /* linear search as this may be ordered
-     * or a set of two ordered arrays */
+    /* 
+     * perform linear search as this list is ordered
+     * or a set of two ordered lists in sequence 
+     */
     while( s32v_idx < lcache_ptr->s32v_cnt ) {
       if( lcache_ptr->u32p_hash[ s32v_idx ] == u32v_hash ) {
-	u64v_ret = lcache_ptr->u64p_element[ s32v_idx ];
-	break;
+        u64v_ret = lcache_ptr->u64p_element[ s32v_idx ];
+        break;
       }
       s32v_idx++;
     }
@@ -153,3 +187,7 @@ u64_t u64f_lcache_find( lcache_t* lcache_ptr, u32_t u32v_hash )
   
   return u64v_ret;
 }
+
+/*
+ * end: lcache.c
+ **/
