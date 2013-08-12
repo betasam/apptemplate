61482f9dd71664adb6007b99eca1e1f4d5133559
Sunil Beta <betasam@gmail.com>
2013-08-12 08:02:04 +0530

    added: comments apropos revised: variable names to fit with 'Coding Guidelines' 	 see (doc/CODING) for more info. formatted: removed TAB character

--- src/main.c
+++ src/main.c
@@ -1,16 +1,22 @@
 /**
- * @file	main.c
+ * @file        main.c
+ * @brief       actual entry point used during execution
  */
 
 #include <ltypes.h>
 
 #include <fibonacci_app.h>
 
+/**
+ * @function    main
+ * @brief       POSIX standard __entry
+ * @return      0 on success -1 on failure
+ */
 int main( int argc, char *argv[] )
 {
-  int retval = 0;
-  retval = s32f_fibonacci_app( argc, argv );
-  return retval;
+  int s32v_retval = 0;
+  s32v_retval = s32f_fibonacci_app( argc, argv );
+  return s32v_retval;
 }
 
 /* 
