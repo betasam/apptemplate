bd3c52e85d83c191a1463759b6fcd95ee8510603
Sunil Beta <betasam@gmail.com>
2013-08-12 09:08:12 +0530

    formatted:	source according to coding guidelines

--- src/lconfig.c
+++ src/lconfig.c
@@ -1,6 +1,6 @@
 /**
- * @file	lconfig.c
- * @brief	handle arguments, etc.
+ * @file        lconfig.c
+ * @brief       handle arguments, etc.
  */
 
 #include <unistd.h>
@@ -10,13 +10,21 @@
 #include <ltypes.h>
 #include <lconfig.h>
 
-/**
+/*
  *  POSIX adherence 
  *   getopt.h defines:
- *	extern char *optarg;
- *	extern int   optind, opterr, optopt;
+ *      extern char *optarg;
+ *      extern int   optind, opterr, optopt;
  */
 
+/**
+ * @function    s32f_parse_args
+ * @brief       parse command line arguments, extract number
+ * @param       argc: POSIX (int)
+ * @param       argv: POSIX (char**)
+ * @param       u32p_num        pointer to number
+ * @return      -1 on failure, +ve FLAGS on success. refer: lconfig.h
+ */
 s32_t s32f_parse_args( int argc, char *argv[], u32_t* u32p_num )
 {
   s32_t s32v_ret = 0;
@@ -34,23 +42,23 @@ s32_t s32f_parse_args( int argc, char *argv[], u32_t* u32p_num )
       switch( s32v_opt ) {
 
       case CHRC_ARG_NUMBER:
-	u32v_num =  strtoul(optarg, NULL, INTC_DEFAULT_BASE);
-	s32v_ret |= INTC_FLAG_NUMBER;
-	break;
+        u32v_num =  strtoul(optarg, NULL, INTC_DEFAULT_BASE);
+        s32v_ret |= INTC_FLAG_NUMBER;
+        break;
       case CHRC_ARG_RECURSE:
-	s32v_ret |= INTC_FLAG_RECURSE;
-	break;
+        s32v_ret |= INTC_FLAG_RECURSE;
+        break;
       case CHRC_ARG_CACHE:
-	s32v_ret |= INTC_FLAG_CACHE;
-	break;
+        s32v_ret |= INTC_FLAG_CACHE;
+        break;
       case CHRC_ARG_STAMP:
-	s32v_ret |= INTC_FLAG_STAMP;
-	break;
+        s32v_ret |= INTC_FLAG_STAMP;
+        break;
       case CHRC_ARG_HELP:
-	s32v_ret |= INTC_FLAG_HELP;
-	break;
+        s32v_ret |= INTC_FLAG_HELP;
+        break;
       default:
-	s32v_ret |= INTC_FLAG_HELP;	
+        s32v_ret |= INTC_FLAG_HELP;     
 
       }
     }
@@ -61,3 +69,7 @@ s32_t s32f_parse_args( int argc, char *argv[], u32_t* u32p_num )
 
   return( s32v_ret );
 }
+
+/*
+ * end: lconfig.c
+ **/
